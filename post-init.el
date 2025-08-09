da5b10b48b12159140a75c584c37a81f57648745
Sebgabones <133339614+Sebagabones@users.noreply.github.com>
2025-08-09 22:47:09 +0800

    removed commented out chunk

--- post-init.el
+++ post-init.el
@@ -854,19 +854,7 @@
   (setq flycheck-highlighting-mode "lines")
   (setq lsp-diagnostics-provider :none)
   )
-;; (flycheck-define-checker
-;;     python-mypy ""
-;;     :command ("mypy"
-;;               "--ignore-missing-imports" "--strict"
-;;               "--python-version" "3.12"
-;;               source-original)
-;;     :error-patterns
-;;     ((error line-start (file-name) ":" line ": error:" (message) line-end))
-;;     :modes python-mode)
-;;
-;;
-;; (add-to-list 'flycheck-checkers 'python-mypy t)
-;; (flycheck-add-next-checker 'python-pylint 'python-mypy t))
+
 
 (use-package flycheck-inline
   :after flycheck
