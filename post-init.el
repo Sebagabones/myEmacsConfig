11b01f06487d1e243b10cdacf50f304702f804ff
Sebgabones <133339614+Sebagabones@users.noreply.github.com>
2025-10-20 21:10:21 +0800

    hydra for mergeing

--- post-init.el
+++ post-init.el
@@ -1045,6 +1045,48 @@
   (transient-append-suffix 'magit-diff '(-1 -1 -1)
     '("D" "Toggle magit-delta" myfun/toggle-magit-delta))) ;Borrowed from https://shivjm.blog/better-magit-diffs/
 
+
+(use-package hydra
+  :after magit
+  :config                               ;From https://github.com/alphapapa/unpackaged.el#smerge-mode
+  (defhydra unpackaged/smerge-hydra
+    (:color pink :hint nil :post (smerge-auto-leave))
+    "
+^Move^       ^Keep^               ^Diff^                 ^Other^
+^^-----------^^-------------------^^---------------------^^-------
+_n_ext       _b_ase               _<_: upper/base        _C_ombine
+_p_rev       _u_pper              _=_: upper/lower       _r_esolve
+^^           _l_ower              _>_: base/lower        _k_ill current
+^^           _a_ll                _R_efine
+^^           _RET_: current       _E_diff
+"
+    ("n" smerge-next)
+    ("p" smerge-prev)
+    ("b" smerge-keep-base)
+    ("u" smerge-keep-upper)
+    ("l" smerge-keep-lower)
+    ("a" smerge-keep-all)
+    ("RET" smerge-keep-current)
+    ("\C-m" smerge-keep-current)
+    ("<" smerge-diff-base-upper)
+    ("=" smerge-diff-upper-lower)
+    (">" smerge-diff-base-lower)
+    ("R" smerge-refine)
+    ("E" smerge-ediff)
+    ("C" smerge-combine-with-next)
+    ("r" smerge-resolve)
+    ("k" smerge-kill-current)
+    ("ZZ" (lambda ()
+            (interactive)
+            (save-buffer)
+            (bury-buffer))
+     "Save and bury buffer" :color blue)
+    ("q" nil "cancel" :color blue))
+  :hook (magit-diff-visit-file . (lambda ()
+                                   (when smerge-mode
+                                     (unpackaged/smerge-hydra/body)))))
+
+
 (use-package exec-path-from-shell
   :defer t
   :init
