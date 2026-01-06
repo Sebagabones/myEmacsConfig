7dd2e355d0e2e7f277dccf1ca5fcbf0a0ddbf521
Sebgabones <133339614+Sebagabones@users.noreply.github.com>
2026-01-06 20:02:16 +0800

    tree-sitter config

--- post-init.el
+++ post-init.el
@@ -685,9 +685,16 @@
   :custom
   (treesit-auto-install 'prompt)
   :config
-  (treesit-auto-add-to-auto-mode-alist 'all)
+  (delete 'cpp treesit-auto-langs)
+  (delete 'c treesit-auto-langs)
+  (treesit-auto-add-to-auto-mode-alist '((awk bash bibtex blueprint c-sharp clojure cmake commonlisp css dart
+                                              dockerfile elixir glsl go gomod heex html janet java javascript json julia
+                                              kotlin latex lua magik make markdown nix nu org perl proto python r ruby
+                                              rust scala sql surface toml tsx typescript typst verilog vhdl vue wast wat
+                                              wgsl yaml)))
   (setopt treesit-font-lock-level 4)
-  (global-treesit-auto-mode))
+  (global-treesit-auto-mode)
+  )
 
 ;; A file and project explorer for Emacs that displays a structured tree
 ;; layout, similar to file browsers in modern IDEs. It functions as a sidebar
